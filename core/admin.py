7d495a91d463218dc6838b6eadbbccae87018ceb
israel <israelwerther48@outlook.com>
2020-01-04 17:17:19 -0300

    Aula 168 concluida

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Marca, Veiculo, Pessoa, Parametros
+from .models import (
+    Marca, 
+    Veiculo, 
+    Pessoa, 
+    Parametros, 
+    MovRotativo
+)
 
 
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
+admin.site.register(MovRotativo)
 
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class Pessoa(models.Model):
-    nome         = models.CharField(max_length=100)
-    endereco     = models.CharField(max_length=200)
-    telefone     = models.CharField(max_length=20)
+    nome            = models.CharField(max_length=100)
+    endereco        = models.CharField(max_length=200)
+    telefone        = models.CharField(max_length=20)
 
     def __str__(self):
         return self.nome
 
 
 class Marca(models.Model):
-    nome         = models.CharField(max_length=50)
+    nome            = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome    
 
 
 class Veiculo(models.Model):
-    marca        = models.ForeignKey(Marca, on_delete=models.PROTECT)
-    placa        = models.CharField(max_length=7)
-    proprietario = models.ForeignKey(Pessoa, on_delete=models.PROTECT, blank=True, null=True) #Apenas para não precisar preencher na hora, mas depois verificar qual a necessidade disto >> blank=True,null=True,
-    cor          = models.CharField(max_length=15)
-    observacoes  = models.TextField()
+    marca           = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    placa           = models.CharField(max_length=7)
+    proprietario    = models.ForeignKey(Pessoa, on_delete=models.PROTECT, blank=True, null=True) #Apenas para não precisar preencher na hora, mas depois verificar qual a necessidade disto >> blank=True,null=True,
+    cor             = models.CharField(max_length=15)
+    observacoes     = models.TextField()
 
     def __str__(self):
         return self.marca.nome + ' - ' + self.placa
@@ -32,9 +32,21 @@ class Veiculo(models.Model):
 
 
 class Parametros(models.Model):
-    valor_hora   = models.DecimalField(max_digits=5, decimal_places=2)
-    valor_mes    = models.DecimalField(max_digits=6, decimal_places=2)
+    valor_hora      = models.DecimalField(max_digits=5, decimal_places=2)
+    valor_mes       = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return 'Parametros gerais'
+    
+
+
+class MovRotativo(models.Model):
+    checkin         = models.DateTimeField(auto_now=False)
+    checkout        = models.DateTimeField(auto_now=False, null=True, blank=True)
+    valor_hora      = models.DecimalField(max_digits=5, decimal_places=2)
+    veiculo         = models.ForeignKey(Veiculo, on_delete=models.PROTECT)
+    pago            = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.veiculo.placa
     
