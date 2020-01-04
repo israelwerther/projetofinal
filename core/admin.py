4d050654128966b4013b4d97c91ee8dbf702a58a
israel <israelwerther48@outlook.com>
2020-01-04 17:47:43 -0300

    Aula 169 concluida

--- core/admin.py
+++ core/admin.py
@@ -7,11 +7,14 @@ from .models import (
     MovRotativo
 )
 
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = (
+        'checkin', 'checkout', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
 
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
-admin.site.register(MovRotativo)
+admin.site.register(MovRotativo, MovRotativoAdmin)
 
 # Register your models here.
