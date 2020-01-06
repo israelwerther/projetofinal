c51f2a8edde8f085ed2071b84d91091cd9f6654b
israel <israelwerther48@outlook.com>
2020-01-06 17:15:43 -0300

    Aula 175 concluida

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
-from .views import home, lista_pessoas
+from .views import (
+    home, 
+    lista_pessoas, 
+    lista_veiculos, 
+    lista_movrotativos
+)
 
 urlpatterns = [
     path('', home, name='core_home'),
-    path('pessoas/', lista_pessoas, name='core_lista_pessoas')
-
+    path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
+    path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
+    path('mov-rot-list/', lista_movrotativos, name='core_lista_movrotativos')
 ]
     
