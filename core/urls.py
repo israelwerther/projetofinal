ef975af79d22831e4873befd5bb3343344357aa5
israel <israelwerther48@outlook.com>
2020-01-12 11:22:21 -0300

    Organização do código

--- core/urls.py
+++ core/urls.py
@@ -15,15 +15,20 @@ from .views import (
 
 urlpatterns = [
     path('', home, name='core_home'),
+
     path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
     path('pessoa-novo/', pessoa_novo, name='core_pessoa_novo'),
-    path('veiculo-novo/', veiculo_novo, name='core_veiculo_novo'),
-    path('mov-rot-novo/', movrotativos_novo, name='core_movrotativos_novo'),
-    path('mensalista-novo/', mensalista_novo, name='core_mensalista_novo'),
-    path('mov-mensal-novo/', movmensalista_novo, name='core_movmensalista_novo'),
+
     path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
+    path('veiculo-novo/', veiculo_novo, name='core_veiculo_novo'),
+
     path('mov-rot/', lista_movrotativos, name='core_lista_movrotativos'),
+    path('mov-rot-novo/', movrotativos_novo, name='core_movrotativos_novo'),
+
     path('mensalistas/', lista_mensalistas, name='core_lista_mensalistas'),
+    path('mensalista-novo/', mensalista_novo, name='core_mensalista_novo'),
+
     path('mov-mensal/', lista_movmensalista, name='core_lista_movmensalista'),
+    path('mov-mensal-novo/', movmensalista_novo, name='core_movmensalista_novo'),
 ]
     
